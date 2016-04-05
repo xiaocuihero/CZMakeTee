2dd2abd68978b0912366f12db6af5e4b85fe761e
xiaocuihero <xiaocuihero1@163.com>
2016-04-05 14:24:57 +0800

    Initial Commit

---
+++ CZMakeTee/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CZMakeTee
+//
+//  Created by Cui on 16/4/5.
+//  Copyright © 2016年 Cui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CZMakeTee/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CZMakeTee
+//
+//  Created by Cui on 16/4/5.
+//  Copyright © 2016年 Cui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
